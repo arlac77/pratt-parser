f6c0fcaa9d642b6a5ae176ee4c7ca4b67fc52427
Markus Felten <Markus.Felten@gmx.de>
2016-12-29 15:31:10 +0100

    chore(rollup): only provide es6 module without merging

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 /* jslint node: true, esnext: true */
 'use strict';
 
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   format: 'cjs',
-  plugins: [nodeResolve(), commonjs()]
+  plugins: []
 };
