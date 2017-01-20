6a3e0f9dcb281dea070d0a239d113fa9de513ca7
Markus Felten <Markus.Felten@gmx.de>
2017-01-20 19:49:26 +0100

    test: LineCommentToken test added

---
+++ tests/line_comment_token_test.js
@@ -0,0 +1,29 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should();
+
+const {
+  LineCommentToken
+} = require('../dist/parser');
+
+describe('tokens', () => {
+  const tokenizer = {};
+
+  describe('line comment', () => {
+    const pp = {
+      chunk: 'x#   \n  A',
+      offset: 1,
+      lineNumber: 1
+    };
+
+    it('delivers undefined', () => assert.equal(undefined, LineCommentToken.parseString(tokenizer, pp)));
+    it('moved forward', () => assert.equal(5, pp.offset));
+    it('increased lineNumber', () => assert.equal(2, pp.lineNumber));
+  });
+});
