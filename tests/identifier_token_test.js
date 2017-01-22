851d2063e122d3f014ff83e9d9b1bcc2dc2198f7
Markus Felten <Markus.Felten@gmx.de>
2017-01-22 01:13:11 +0100

    test: identifier test

---
+++ tests/identifier_token_test.js
@@ -0,0 +1,33 @@
+/* global describe, it, xit */
+/* jslint node: true, esnext: true */
+
+'use strict';
+
+const chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should();
+
+const {
+  IdentifierToken
+} = require('../dist/parser');
+
+describe('tokens', () => {
+  const tokenizer = {};
+
+  describe('identifier', () => {
+    const pp = {
+      chunk: ' abc   \n  A',
+      offset: 1,
+      lineNumber: 1,
+      get properties() {
+        return {};
+      }
+    };
+
+    const t = IdentifierToken.parseString(tokenizer, pp);
+
+    it('delivers undefined', () => assert.equal('abc', t.value));
+    it('moved forward', () => assert.equal(4, pp.offset));
+  });
+});
