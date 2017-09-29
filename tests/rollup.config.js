da8d35f59b3ffc9eb5cd4d8e6d464e05b2287d7d
Markus Felten <Markus.Felten@gmx.de>
2017-09-29 13:21:37 +0200

    chore(rollup): update from template chore(rollup): update from template chore(scripts): update prepare@rollup -c from template chore(devDependencies): update rollup@^0.50.0 from template chore(devDependencies): add rollup-plugin-commonjs@^8.2.1 from template chore(devDependencies): add rollup-plugin-node-resolve@^3.0.0 from template chore(devDependencies): update semantic-release@^8.0.4 from template chore(engines): update node@>=8.6.0 from template docs(README): update from template chore(travis): merge from template .travis.yml

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -2,8 +2,9 @@ import babel from 'rollup-plugin-babel';
 import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  entry: 'tests/**/*-test.js',
+  input: 'tests/**/*-test.js',
   external: ['ava'],
+
   plugins: [
     babel({
       babelrc: false,
@@ -12,7 +13,10 @@ export default {
     }),
     multiEntry()
   ],
-  format: 'cjs',
-  dest: 'build/test-bundle.js',
-  sourceMap: true
+
+  output: {
+    file: 'build/test-bundle.js',
+    format: 'cjs',
+    sourcemap: true
+  }
 };
