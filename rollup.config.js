165b25952b591566dcc6d993f35e466944090f98
Markus Felten <Markus.Felten@gmx.de>
2017-10-12 21:02:29 +0200

    chore(scripts): update prepare@rollup -c from template chore(devDependencies): update rollup@^0.50.0 from template chore(devDependencies): add rollup-plugin-commonjs@^8.2.1 from template chore(devDependencies): add rollup-plugin-node-resolve@^3.0.0 from template chore(devDependencies): update semantic-release@^8.1.2 from template chore(engines): update node@>=8.7.0 from template chore(travis): merge from template .travis.yml docs(README): update from template chore(rollup): update from template chore(rollup): update from template chore(npm): update .npmignore from template

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,18 @@ import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 export default {
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'cjs'
-    }
-  ],
+  output: {
+    file: pkg.main,
+    format: 'cjs'
+  },
+
   plugins: [
     babel({
       babelrc: false,
       presets: ['stage-3'],
       exclude: 'node_modules/**'
     })
-  ]
+  ],
+
+  input: pkg.module
 };
