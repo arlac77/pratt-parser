66802b81a64aedc5d6e8dabf8faf819a417f46d1
Markus Felten <Markus.Felten@gmx.de>
2018-08-06 01:16:26 +0200

    chore(package): add rollup-plugin-cleanup@^3.0.0 chore(package): semantic-release@^15.9.5 chore(rollup): import cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import cleanup from 'rollup-plugin-cleanup';
 import executable from 'rollup-plugin-executable';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
